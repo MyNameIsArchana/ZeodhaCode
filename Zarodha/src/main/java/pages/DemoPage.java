1a4ae74779b6c0f7bcb27ab47a518110783021a4
MyNameIsArchana <archanawahatule16@gmail.com>
2024-03-04 21:47:37 +0530

    new change made in display method

--- Zarodha/src/main/java/pages/DemoPage.java
+++ Zarodha/src/main/java/pages/DemoPage.java
@@ -16,6 +16,7 @@ public class DemoPage extends FrameworkBase
 	public void display() 
 	  {
 		System.out.println("I have made changes in the code");
+		System.out.println("second change is made");
 	  }
 
 }
